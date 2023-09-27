9b149fb5e2577cc5c2eb353f27a1b3c483719411
najeebib <najeebibraheem1999@hotmail.com>
2023-09-27 12:17:15 +0300

    Add food and score tracking

--- snake.py
+++ snake.py
@@ -1,11 +1,12 @@
-from turtle import Screen, Turtle
+from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
-UP =90
+UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
+
 
 class Snake:
     def __init__(self):
